37901c5f22e4845b2c9e659945327a8da438248a
milanidk15 <milannawzad008@gmail.com>
2025-01-17 05:56:44 +0300

    Update README.md

---
+++ calc/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        int plus = 1 + 1;
+        System.out.println(plus);
+    }
+}
